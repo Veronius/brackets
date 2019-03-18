7d8283b1d041b55fc4ebe43c5680b96cf491e12d
Ilya <i.veroniys@gmail.com>
2019-03-18 21:27:57 +0300

    feat: add function, which is balancing brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+        let stack = [],
+            stack2 = [],
+            stack3 = [],
+            pair = {
+                '(': ')',
+                '[': ']',
+                '{': '}',
+                '1': '2',
+                '3': '4',
+                '5': '6',
+
+                '8': '0+8',
+            };
+    let ch;
+        for (i = 0; i < str.length; i++) {
+            ch = str.charAt(i);
+            switch (ch) {
+                case '(':
+                case '[':
+                case '{':
+                case '1':
+                case '3':
+                case '5':
+                case '8':
+
+                    stack.push(pair[ch]);
+                    break;
+
+                case ')':
+                case ']':
+                case '}':
+                case '2':
+                case '4':
+                case '6':
+
+                    if (0 == stack.length)
+                        return false;
+
+                    if (stack[stack.length - 1] != ch)
+                        return false;
+
+                    stack.pop();
+
+
+            };
+
+        };
+        if (stack.length)
+            return false;
+        return true;
+};
